90647ba95790d812fb16f8f6f6a2b5f2153e67d2
NikolaKasabov <n_kasabov@mail.bg>
2024-04-18 17:18:53 +0300

    add hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Header from "@/components/Header";
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main>
+        <Hero />
+      </main>
     </>
   );
 }
