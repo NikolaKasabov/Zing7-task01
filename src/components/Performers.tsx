e3a1aaa270ead84e6f60966a7ddb7277620e6496
NikolaKasabov <n_kasabov@mail.bg>
2024-04-20 13:30:53 +0300

    code refactoring

--- src/components/Performers.tsx
+++ src/components/Performers.tsx
@@ -5,26 +5,41 @@ import performer3 from "../../public/images/performer3.png";
 import performer4 from "../../public/images/performer4.png";
 
 export default function Performers() {
+  const performersInfo = [
+    {
+      id: 1,
+      name: "Fat Joe",
+      image: performer1,
+    },
+    {
+      id: 2,
+      name: "Eminem",
+      image: performer2,
+    },
+    {
+      id: 3,
+      name: "50 cent",
+      image: performer3,
+    },
+    {
+      id: 4,
+      name: "Jay-Z",
+      image: performer4,
+    },
+  ];
+
   return (
     <>
       <h3 className="mt-12 text-lg font-bold text-center xl:text-left">Изпълнители</h3>
       <div className="mt-6 flex flex-wrap justify-center gap-[15px] text-black">
-        <div>
-          <Image src={performer1} alt="Fat Joe" />
-          <p className="mt-2 text-sm text-center">Fat Joe</p>
-        </div>
-        <div>
-          <Image src={performer2} alt="Eminem" />
-          <p className="mt-2 text-sm text-center">Eminem</p>
-        </div>
-        <div>
-          <Image src={performer3} alt="50 cent" />
-          <p className="mt-2 text-sm text-center">50 cent</p>
-        </div>
-        <div>
-          <Image src={performer4} alt="Jay-Z" />
-          <p className="mt-2 text-sm text-center">Jay-Z</p>
-        </div>
+        {performersInfo.map(performer => {
+          return (
+            <div key={performer.id}>
+              <Image src={performer.image} alt={performer.name} />
+              <p className="mt-2 text-sm text-center">{performer.name}</p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
